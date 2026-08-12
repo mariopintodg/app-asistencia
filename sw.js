a76a403862042c59d61f6268ea4baba2d9761a37
mariopintodg <mariopintodg@gmail.com>
2026-08-12 19:33:46 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'asistencia-app-v54';
+const CACHE_NAME = 'asistencia-app-v55';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './styles.css?v=34',
-  './app.js?v=31',
+  './app.js?v=32',
   './manifest.json',
   './icon.svg',
   './logo-empresa.png',
